d82ebace29ce7c7d771cf5d62d0e734e536ff04a
Blaster4385 <venkateshchaturvedi12@gmail.com>
2021-06-01 18:05:43 +0530

    Adapt trees to RMX1851 - Replace proprietary blobs with RMX1851 - Imported some init scripts - Rename RMX1971 to RMX1851 - Imported extract-files.sh and setup-makefiles.sh from https://github.com/PixelExperience-Devices/device_realme_RMX1851

--- aosp_RMX1851.mk
+++ aosp_RMX1851.mk
@@ -7,7 +7,7 @@
 # Inherit some common Raven OS stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Inherit from RMX1971 device.
+# Inherit from RMX1851 device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -27,19 +27,14 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_DEVICE := RMX1971
-PRODUCT_NAME := aosp_RMX1971
+PRODUCT_DEVICE := RMX1851
+PRODUCT_NAME := aosp_RMX1851
 PRODUCT_BRAND := Realme
-PRODUCT_MODEL := Realme 5 Pro
+PRODUCT_MODEL := Realme 3 Pro
 PRODUCT_MANUFACTURER := Realme
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220105.007 8030436 release-keys"
-    BUILD_FINGERPRINT="google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys"
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="RMX1971" \
-    TARGET_DEVICE="RMX1971"
-
+    PRODUCT_NAME="RMX1851" \
+    TARGET_DEVICE="RMX1851"
